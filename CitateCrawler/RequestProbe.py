86f7cac983ee4f3f78ee63386ca93cf739ffa36f
mikhail <mihail.melnik.ifmo@gmail.com>
2015-03-18 02:27:05 +0300

    little commit

--- CitateCrawler/RequestProbe.py
+++ CitateCrawler/RequestProbe.py
@@ -1,1 +1,18 @@
 __author__ = 'Mishanya'
+
+import urllib3
+
+#from urllib.request import Request, urlopen
+"""
+q = Request('http://api.elsevier.com/content/search/index:SCIDIR-OBJECT?query=DOI(10.1016/j.bbabio.2008.10.005)')
+q.add_header('X-ELS-APIKey', 'b4ecc27393a3e69e638f5efe599787ab')
+q.add_header('X-ELS-ResourceVersion', 'XOCS')
+q.add_header('Accept', 'text/xml')
+a = urlopen(q).read()
+"""
+http = urllib3.PoolManager()
+
+headers = urllib3.make_headers({'X-ELS-APIKey': 'b4ecc27393a3e69e638f5efe599787ab', 'X-ELS-ResourceVersion': 'XOCS', 'Accept': 'text/xml'})
+req = http.request('GET', 'http://api.elsevier.com/content/search/index:SCIDIR-OBJECT?query=DOI(10.1016/j.bbabio.2008.10.005)', headers=headers)
+print(req.status)
+print(req)
