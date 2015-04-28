04c561cc4068defce7482bfd4027ab778c43542f
mikhail <mihail.melnik.ifmo@gmail.com>
2015-04-28 09:32:35 +0300

    clean crawler

--- CitateCrawler/ScopusCrawler.py
+++ CitateCrawler/ScopusCrawler.py
@@ -10,98 +10,6 @@ import os
 import os.path
 import time
 
-start = time.clock()
-
-api_key = 'b4ecc27393a3e69e638f5efe599787ab'
-
-# Request' headers
-headers = dict()
-headers['X-ELS-APIKey'] = api_key
-headers['X-ELS-ResourceVersion'] = 'XOCS'
-headers['Accept'] = 'application/json'
-
-api_resource = "https://api.elsevier.com/content/search/scopus?"
-""" Searching parameters """
-# params = 'query=title-abs-key(porn video AND NOT gay)&sort=-citedby-count'
-params = 'query=title-abs-key((anal sex) AND NOT gay)'
-
-fields = '&field=link ref=self,link ref=scopus,link ref=scopus-citedby,' \
-         'prism:url,dc:identifier,eid,dc:title,prism:aggregationType,' \
-         'citedby-count,prism:publicationName,prism:isbn,prism:issn,' \
-         'prism:volume,prism:issueIdentifier,prism:pageRange,prism:coverDate,prism:coverDisplayDate,' \
-         'prism:doi,pubmed-id,orcid,dc:creator,' \
-         'affilname,affiliation-city,affiliation-country,afid,affiliation-url,name-variant,' \
-         'dc:description,authkeywords,intid,article-number'
-fields = '&field=link ref=self,link ref=scopus,link ref=scopus-citedby,' \
-         'prism:url,dc:identifier,eid,dc:title,prism:aggregationType,' \
-         'citedby-count,prism:publicationName,prism:isbn,prism:issn,' \
-         'prism:volume,prism:issueIdentifier,prism:pageRange,prism:coverDate,prism:coverDisplayDate,' \
-         'prism:doi,pubmed-id,orcid,dc:creator,' \
-         'affilname,affiliation-city,affiliation-country,afid,affiliation-url,name-variant,' \
-         'dc:description,authkeywords,intid,article-number'
-# fields = ''
-# Result request' url
-url = api_resource + params
-
-# Maximum amount of papers
-max_papers = 20
-# Max recursion deep
-max_deep = 2
-# Print article data, during crawling process
-need_print = False
-
-# Dictionaries for data save
-paper_ids = dict()
-paper_edges = dict()
-
-# Create files
-json_dir = "./articles_json/"
-data_dir = "./articles_data/"
-if not os.path.exists(json_dir):
-    os.makedirs(json_dir)
-if not os.path.exists(data_dir):
-    os.makedirs(data_dir)
-# Create data files
-ids_file = open("./articles_data/ids.txt", 'w')
-edge_file = open("./articles_data/edges.txt", 'w')
-cit_file = open("./articles_data/cit_counts.txt", 'w')
-auth_file = open("./articles_data/authors.txt", 'w')
-key_file = open("./articles_data/keys.txt", 'w')
-date_file = open("./articles_data/years.txt", 'w')
-# Create json file
-dt = time.strftime("%d%b%Y%H%M", time.gmtime())
-article_file = open("./articles_json/" + dt + ".txt", 'w')
-
-print("Preprocessing time = " + str(time.clock() - start))
-
-# Main response (Search by parameters)
-print("First search started")
-start = time.clock()
-response = requests.get(url, headers=headers)
-print("First search request: " + str(time.clock() - start))
-
-response_result = json.loads(response.content.decode("utf-8"))
-
-# Page content (Articles on the first page)
-print("Page content started")
-start = time.clock()
-page_url = response_result['search-results']['link'][0]['@href'].replace("http", "https").replace(":80", "") + "&view=STANDARD" +fields
-print("Page_articles request: " + str(time.clock() - start))
-page_response = requests.get(page_url, headers=headers)
-page_result = json.loads(page_response.content.decode("utf-8"))
-
-# First list
-articles_list = page_result['search-results']['entry']
-
-# Start index
-cur_paper = 0
-
-def correct_string(name):
-    shit_cymbols = {"\xe1": "a", '\xe8': 'e'}
-    for cymb in shit_cymbols:
-        name.replace(cymb, shit_cymbols[cymb])
-    return name
-
 def crawling(articles, cur_deep):
     """
     Recursive function for crawling.
@@ -111,9 +19,12 @@ def crawling(articles, cur_deep):
     :param cur_deep: Current recursion deep
     """
     global cur_paper
-    print("Start crawling at level " + str(cur_deep))
-
+    # print("Start crawling at level " + str(cur_deep))
+    start = time.clock()
     for article in articles:
+        finish = time.clock()
+        print("Article time = " + str(finish - start))
+        start = finish
         if len(paper_ids) >= max_papers:
             break
 
@@ -121,38 +32,28 @@ def crawling(articles, cur_deep):
         title = article['dc:title']
         if title in paper_ids.keys():
             continue
-        cit_count = 0
         try:
             cit_count = article['citedby-count']
-        except Exception:
-            pass
-        authors = ""
-        try:
             authors = article['dc:creator']
-        except Exception:
-            pass
-        date = ""
-        try:
             date = article['prism:coverDisplayDate']
         except Exception:
-            pass
+            print("WRONG ARTICLE!!!")
+            continue
 
         article_url = article['prism:url'].replace("http", "https").replace(":80", "")
 
         # Find keywords of this article
-        keywords = []
-        # try:
-        #     print("Keywords started")
-        #     start = time.clock()
-        #     article_keywords = requests.get(article_url + "?field=authkeywords", headers=headers)
-        #
-        #     article_keywords = json.loads(article_keywords.content.decode("utf-8"))
-        #     keywords = [keyword['$'] for keyword in article_keywords['abstracts-retrieval-response']['authkeywords']['author-keyword']]
-        #     print("Keywords request: " + str(time.clock() - start))
-        # except Exception:
-        #     print("Keywords request FAIL: " + str(time.clock() - start))
-        #     pass
-        # Add keywords into article json
+        try:
+            # print("Keywords started")
+            article_keywords = requests.get(article_url + "?field=authkeywords", headers=headers)
+
+            article_keywords = json.loads(article_keywords.content.decode("utf-8"))
+            keywords = [keyword['$'] for keyword in article_keywords['abstracts-retrieval-response']['authkeywords']['author-keyword']]
+        except Exception:
+            print("Key words error")
+            continue
+
+        # Add keywords data
         article['keywords'] = keywords
 
         # Write article data to dictionaries
@@ -162,18 +63,19 @@ def crawling(articles, cur_deep):
         citations = []
         citations_names = []
         have_citations = False
-        if cur_deep < max_deep:
+        if cur_deep < max_deep and not cit_count == '0':
             try:
-                print("Citations search started")
+                # print("Citations search started")
                 start = time.clock()
                 citations_response = requests.get(api_resource + 'query=refeid(' + str(article['eid']) + ')', headers=headers)
-                print("Citations request: " + str(time.clock() - start))
+                # print("Citations request: " + str(time.clock() - start))
                 citations_result = json.loads(citations_response.content.decode("utf-8"))
                 citations = citations_result['search-results']['entry']
                 # Add edges
                 citations_names = [citation['dc:title'] for citation in citations][:max_papers-cur_paper]
                 have_citations = True
             except Exception:
+                print("citations error")
                 pass
 
         # Print current article data, if need
@@ -188,83 +90,120 @@ def crawling(articles, cur_deep):
 
         # Add citations into article json
         article['citations'] = citations_names
-        paper_edges[cur_paper] = citations_names
-
-        # Write other data in files
-        try:
-            ids_file.write(str(cur_paper) + "\t" + title + "\n")
-            try:
-                auth_file.write(str(cur_paper) + "\t" + correct_string(authors) + "\n")
-            except Exception:
-                auth_file.write(str(cur_paper) + "\t" + "" + "\n")
-            try:
-                date_file.write(str(cur_paper) + "\t" + date + "\n")
-            except Exception:
-                date_file.write(str(cur_paper) + "\t" + "" + "\n")
-            try:
-                cit_file.write(str(cur_paper) + "\t" + str(cit_count) + "\n")
-            except Exception:
-                cit_file.write(str(cur_paper) + "\t" + "" + "\n")
-            try:
-                key_file.write(str(cur_paper) + "\t" + str(keywords) + "\n")
-            except Exception:
-                key_file.write(str(cur_paper) + "\t" + "" + "\n")
-            try:
-                edge_file.write(str(cur_paper) + "\t" + str(citations_names) + "\n")
-            except Exception:
-                edge_file.write(str(cur_paper) + "\t" + "" + "\n")
-        except Exception:
-            print("WRONG TITLE!!!")
-            del(paper_ids[title])
-            continue
 
         # Write current json into file
         json.dump(article, article_file)
         article_file.write("\n")
+        print("article has been written")
 
         # Increase papers' counter
         cur_paper += 1
 
         # Continue crawling, if this article has citations
-        if cur_deep < max_deep and len(paper_ids) < max_papers and have_citations:
-            crawling(citations, cur_deep + 1)
+        try:
+            if cur_deep < max_deep and len(paper_ids) < max_papers and have_citations:
+                crawling(citations, cur_deep + 1)
+        except Exception:
+            print("Deep crawling error")
+            citations_names = []
 
         pass
     pass
+""" END FUNCTION """
 
+if __name__ == "__main__":
 
-""" Start crawling """
-try:
-    crawling(articles_list, 0)
-    # Continue with next found pages
-    if len(paper_ids) < max_papers and len(response_result['search-results']['link'][2]) > 3:
-        # Next page content
-        page_url = page_result['search-results']['link'][2]['@href'].replace("http", "https").replace(":80", "")
-        page_response = requests.get(page_url, headers=headers)
-        page_result = json.loads(page_response.content.decode("utf-8"))
-        articles_list = page_result['search-results']['entry']
-        crawling(articles_list, 0)
-except Exception:
+    total_start_time = time.clock()
+
+    api_key = 'b4ecc27393a3e69e638f5efe599787ab'
+
+    # Request' headers
+    headers = dict()
+    headers['X-ELS-APIKey'] = api_key
+    headers['X-ELS-ResourceVersion'] = 'XOCS'
+    headers['Accept'] = 'application/json'
+
+    api_resource = "https://api.elsevier.com/content/search/scopus?"
+    # api_resource = "http://api.elsevier.com/content/search/index:SCOPUS?"
+    """ Searching parameters """
+    search_params = ['query=title-abs-key(harmonic search)', 'query=title-abs-key(machine learning)',
+                     'query=title-abs-key(gravitation search algorithm)', 'query=title-abs-key(hyper-heuristics)',
+                     'query=title-abs-key(mapreduce)']
+
+    # Maximum amount of papers
+    max_papers = 20000
+    # Max recursion deep
+    max_deep = 4
+    # Print article data, during crawling process
+    need_print = False
+
+    # Dictionaries for data save
+    paper_ids = dict()
+
+    # Create files
+    json_dir = "./articles_json/"
+    if not os.path.exists(json_dir):
+        os.makedirs(json_dir)
+    # Create data files
+    dt = time.strftime("%d%b%Y%H%M", time.gmtime())
+    article_file = open("./articles_json/" + dt + ".txt", 'w')
+
+    # Start index
+    cur_paper = 0
+
+    fields = '&field=authkeywords'
+    # fields = ''
+
+    for search_query in search_params:
+        print("SEARCH QUERY = " + search_query)
+        if cur_paper >= max_papers:
+            break
+        # Result request' url
+        url = api_resource + search_query + "&sort=citedby-count"
+
+        # Main response (Search by parameters)
+        try:
+            print("First search started")
+            response = requests.get(url, headers=headers)
+
+            response_result = json.loads(response.content.decode("utf-8"))
+
+            # Page content (Articles on the first page)
+            print("Page content started")
+            page_url = response_result['search-results']['link'][0]['@href'].replace("http", "https").replace(":80", "")
+            page_response = requests.get(page_url, headers=headers)
+            page_result = json.loads(page_response.content.decode("utf-8"))
+
+            # First list
+            articles_list = page_result['search-results']['entry']
+            print("First search finished successful")
+        except Exception:
+            print("Error during first search")
+            continue
+
+        """ Start crawling """
+        try:
+            print("Start crawling FIRST page")
+            crawling(articles_list, 0)
+            print("PAGE FINISH")
+            # Continue with next found pages
+            if len(paper_ids) < max_papers and len(response_result['search-results']['link'][2]) > 3:
+                # Next page content
+                print("Start crawling NEXT page")
+                page_url = page_result['search-results']['link'][2]['@href'].replace("http", "https").replace(":80", "")
+                page_response = requests.get(page_url, headers=headers)
+                page_result = json.loads(page_response.content.decode("utf-8"))
+                articles_list = page_result['search-results']['entry']
+                crawling(articles_list, 0)
+                print("PAGE FINISH")
+        except Exception:
+            print("!!! global crawling exception !!!")
+            pass
+
+    # Close files
+    article_file.close()
+
+    finish_time = time.clock()
+    print("ARTICLES COUNT = " + str(cur_paper))
+    print("FULL CRAWLING TIME = " + str(finish_time - total_start_time))
     pass
-
-# Close files
-article_file.close()
-ids_file.close()
-edge_file.close()
-cit_file.close()
-auth_file.close()
-key_file.close()
-date_file.close()
-
-# Convert edges to indexes
-idx_edges_file = open("./articles_data/idx_edges.txt", 'w')
-for title, idx in paper_ids.items():
-    idx_edges_file.write(str(idx) + "\n\t")
-    edges = paper_edges[idx]
-    for edge in edges:
-        if edge in paper_ids.keys():
-            idx_edges_file.write(str(paper_ids[edge]) + "\t")
-    idx_edges_file.write("\n")
-idx_edges_file.close()
-
-pass
