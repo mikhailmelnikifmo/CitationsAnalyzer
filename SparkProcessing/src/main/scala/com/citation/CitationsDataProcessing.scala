a8e899dc4b4b16421a2c204bf1835054da07713d
mikhail <mihail.melnik.ifmo@gmail.com>
2015-05-27 18:52:30 +0300

    refactor

--- SparkProcessing/src/main/scala/com/citation/CitationsDataProcessing.scala
+++ SparkProcessing/src/main/scala/com/citation/CitationsDataProcessing.scala
@@ -1,9 +1,3 @@
-/*
- * Copyright 2015 Sanford Ryza, Uri Laserson, Sean Owen and Joshua Wills
- *
- * See LICENSE file for further information.
- */
-
 package com.citation
 
 import java.lang.Double.isNaN
@@ -19,17 +13,36 @@ object CitationsDataProcessing extends Serializable {
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setAppName("citation")
     val sc = new SparkContext(sparkConf)
-
+    println("privet")
     val directory = "/home/vagrant/citations/"
-//    val jsons = sc.textFile("hdfs://" + directory + "articles_json/*").flatMap(_.split("\n")).
+
+//    println(directory)
+//    val jsons = sc.textFile("hdfs://" + directory + "articles_json/comp_science/2011/articles0 - 500").flatMap(_.split("\n")).
 //                        map(x => JSON.parseFull(x))
-    writeToHdfs(sparkConf, directory + "test_output.txt", "privet")
+//    jsons.take(10).foreach(println)
+//    println("articles")
+//    try {
+//      val articles = jsons.map(x => x.get("dc:title"))
+//    val sizes = jsons.map(x => x.size)
+//    println("sizes")
+//    sizes.take(10).foreach(println)
+//      articles.take(10).foreach(println)
+//    } catch {
+//      case e:Exception => println("opa")
+//    }
+//    jsons.saveAsTextFile("hdfs://" + directory + "json_out4/")
+//    jsons.coalesce(1, true).saveAsTextFile("hdfs://" + directory + "json_out5/")
+
+      println("Titles:")
+      val titles_file = sc.textFile("hdfs://" + directory + "artData/titles.txt").flatMap(_.split("\n"))
+      val titles = titles_file.map(x => (x.split("\t")(0), x.split("\t")(1)))
+      titles.take(20).foreach(println)
   }
 
   def writeToHdfs(sparkConf:SparkConf, filePath: String, text: String): Unit = {
    val path = new Path(filePath)
     val conf = new Configuration()
-    conf.set("fs.defaultFS", sparkConf.getOption("hdfs.uri").toString)
+    conf.addResource(new Path("/etc/hadoop/conf/core-site.xml"))
     val fs = FileSystem.get(conf)
     val os = fs.create(path)
     os.writeChars(text)
